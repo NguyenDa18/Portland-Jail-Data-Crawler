27ab5b6352f318b5477eb917395dfcc92517afd0
Danh Nguyen <danhnguyen267@gmail.com>
2022-07-20 13:04:07 -0700

    Sorting by booking date and name for updated data

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 MULT_COUNTY_JAIL_BOOKINGS_URL = "http://www.mcso.us/PAID/Home/SearchResults"
+CSV_LOCATION = 'data/data.csv'
 
 response = requests.post(MULT_COUNTY_JAIL_BOOKINGS_URL)
 soup = BeautifulSoup(response.text, 'html.parser')
@@ -13,7 +14,7 @@ table_input = table.prettify()
 table_to_df = pd.read_html(table_input, parse_dates=[1])[0]
 
 # sort by date descending
-table_to_df = table_to_df.sort_values(by='Booking Date', ascending = False)
+table_to_df = table_to_df.sort_values(by=["Booking Date", "Name"], ascending = [False, True])
 
 # write to file
-table_to_df.to_csv('data/data.csv', index=False)
+table_to_df.to_csv(CSV_LOCATION, index=False)
