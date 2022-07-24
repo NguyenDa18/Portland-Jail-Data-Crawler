75bfee51c1c53af6da05abd8f146cfeee198510b
Danh Nguyen <danhnguyen267@gmail.com>
2022-07-23 20:31:26 -0700

    Initializing Scrapy version of project for faster scraping

---
+++ inmates_spider/inmates_spider/spiders/inmates.py
@@ -0,0 +1,18 @@
+import scrapy
+import pandas as pd
+
+class InmateSpider(scrapy.Spider):
+    name = 'inmates'
+
+    def start_requests(self):
+        urls = ["http://www.mcso.us/PAID/Home/SearchResults"]
+        for url in urls:
+            yield scrapy.Request(url = url, 
+                        method="POST",
+                        callback=self.parse_table)
+
+    def parse_table(self, response):
+        table_input = response.css('table').get()
+        table_to_df = pd.read_html(table_input, parse_dates=[1])[0]
+        table_to_df = table_to_df.sort_values(by=['Booking Date', 'Name'], ascending=[ False, True ])
+        table_to_df.to_csv('../../data/inmates.csv')
