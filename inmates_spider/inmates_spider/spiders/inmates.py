67d43554ad896436adda23e4ef64e02fc1d7ca84
Danh Nguyen <danhnguyen267@gmail.com>
2022-07-24 14:28:06 -0700

    Iterating over inmate charges with scrapy

--- inmates_spider/inmates_spider/spiders/inmates.py
+++ inmates_spider/inmates_spider/spiders/inmates.py
@@ -16,3 +16,24 @@ class InmateSpider(scrapy.Spider):
         table_to_df = pd.read_html(table_input, parse_dates=[1])[0]
         table_to_df = table_to_df.sort_values(by=['Booking Date', 'Name'], ascending=[ False, True ])
         table_to_df.to_csv('../../../data/demo.csv')
+
+        inmate_links = response.xpath('//[@class="search-results"]//a/@href').getall()
+        for link in inmate_links:
+            url = response.urljoin(link)
+            yield response.follow(url = url, callback = self.parse_inmate_data)
+
+    def parse_inmate_data(self, response):
+        charge_types = response.css('span.charge-description-display::text').getall()
+        charge_bail_amounts = response.css('span.charge-bail-value::text').getall()
+        charge_statuses = response.css('span.charge-status-value::text').getall()
+        charge_items = []
+        for c_type, c_bail, c_status in zip(charge_types, charge_bail_amounts, charge_statuses):
+            charge_item = {
+                'Type': c_type,
+                'Bail': c_bail,
+                'Status': c_status
+            }
+            charge_items.append(charge_item)
+        yield {
+            'Charges': charge_items
+        }
